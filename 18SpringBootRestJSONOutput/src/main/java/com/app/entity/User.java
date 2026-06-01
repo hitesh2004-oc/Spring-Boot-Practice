ec8406b3ead6c36581d05c72ff4b3994e61db5ad
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-06-01 08:23:12 +0530

    Rest JSON Output Practice

---
+++ 18SpringBootRestJSONOutput/src/main/java/com/app/entity/User.java
@@ -0,0 +1,34 @@
+package com.app.entity;
+
+import java.util.Map;
+import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+            @JsonProperty("user-unq-id")
+            private Integer userId;
+            
+            @JsonProperty("user-first-name")
+            private String userName;
+            private String userRole;
+            
+            // Use @JsonIgnore to avoid a variable/field in JSON operations
+            // (used in project)
+            @JsonIgnore
+            private String password;
+            
+            private Set<String> permissions;
+            //private List<String> permissions;
+            //private String[] permissions;
+
+            private Map<String, String> models;
+            //private Properties models;
+
+}
