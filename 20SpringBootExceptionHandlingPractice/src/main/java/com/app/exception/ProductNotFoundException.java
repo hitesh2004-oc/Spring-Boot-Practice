54b5e6944f74851a5ea028c22a6a5ba76c8b7bb4
Hitesh Malviya <hiteshmalviya955@gmail.com>
2026-08-08 12:55:08 +0530

    Practice Project
    Added comments to clarify the purpose of the exception.

--- 20SpringBootExceptionHandlingPractice/src/main/java/com/app/exception/ProductNotFoundException.java
+++ 20SpringBootExceptionHandlingPractice/src/main/java/com/app/exception/ProductNotFoundException.java
@@ -3,11 +3,11 @@ package com.app.exception;
 public class ProductNotFoundException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
-
+// Product Not Found Exception 
 	public ProductNotFoundException() {
 		super();
 	}
-
+// PNFE Message 
 	public ProductNotFoundException(String message) {
 		super(message);
 	}
